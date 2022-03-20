34a001d12740bb44c790e7c2b31431cedb5e2c75
mrbrelax <mrbrelax56@gmail.com>
2022-03-20 22:05:56 +0700

    OOP PHP Dasar

---
+++ 18_namespace/App/Produk/Game.php
@@ -0,0 +1,33 @@
+<?php
+class Game extends Produk implements InfoProduk{
+    public $waktuMain;
+
+    public function __construct($judul = "judul", 
+                                $penulis = "penulis", 
+                                $penerbit = "penerbit", 
+                                $harga = 0, 
+                                $waktuMain = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getInfo(){
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+
+    
+    // public function setDiskon($diskon){
+    //     $this->diskon = $diskon;
+    // }
+
+    public function getInfoProduk()
+    {
+        $str = "Game : " . $this->getInfo() . " - {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+
+?>
